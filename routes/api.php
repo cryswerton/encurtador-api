12d53ee018c3cc8faa376d75ea2f43fb9fcc6030
cryswerton <cryswertons@gmail.com>
2023-09-23 16:31:19 +0100

    test_get_single_link_success added

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,11 @@ Route::get('/links', function () {
     return response()->json(Link::all());
 });
 
+Route::get('/links/{id}', function ($id) {
+    $link = Link::find($id);
+    return response()->json($link);
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- tests/Feature/LinkApiTest.php
+++ tests/Feature/LinkApiTest.php
@@ -11,7 +11,7 @@ class LinkApiTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_get_links_endpoint()
+    public function test_get_links_success()
     {
 
         $links = Link::factory()->count(3)->create();
@@ -20,5 +20,21 @@ class LinkApiTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertJsonCount(3);
+    }
+
+    public function test_get_single_link_success()
+    {
+
+        $link = Link::factory()->create();
+
+        $response = $this->getJson("/api/links/{$link->id}");
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['id']);
+        $response->assertJsonStructure(['title']);
+        $response->assertJsonStructure(['destination']);
+        $response->assertJsonStructure(['short_link']);
+        $response->assertJsonStructure(['created_at']);
+        $response->assertJsonStructure(['updated_at']);
     }
 }
