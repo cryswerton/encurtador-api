12437aee7ae0479e3a945810a3ac86c5e5889140
cryswerton <cryswertons@gmail.com>
2023-09-24 12:17:22 +0100

    test_patch_link_short_link_not_unique added.

--- tests/Feature/LinkApiTest.php
+++ tests/Feature/LinkApiTest.php
@@ -151,6 +151,22 @@ class LinkApiTest extends TestCase
         $this->assertEquals($dataResponse['title'], $data['title']);
     }
 
+    public function test_patch_link_short_link_not_unique()
+    {
+        $link = Link::factory()->create();
+
+        $data = [
+            'short_link' => $link->short_link,
+        ];
+
+        $response = $this->put("/api/links/{$link->id}", $data);
+
+        $dataResponse = $response->json();
+
+        $response->assertStatus(422);
+        $this->assertNotEmpty($dataResponse['errors']['short_link']);
+    }
+
     public function test_patch_link_not_found()
     {
         $data = [
