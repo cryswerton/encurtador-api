3c157f2e29519cf23cf863d9f510a54e3f4046e5
cryswerton <cryswertons@gmail.com>
2023-10-04 10:30:18 +0100

    added clicks column

--- database/migrations/2023_09_22_184305_create_links_table.php
+++ database/migrations/2023_09_22_184305_create_links_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('destination', 400);
             $table->string('short_link');
+            $table->bigInteger('clicks')->unsigned()->default(0);
             $table->timestamps();
         });
     }
