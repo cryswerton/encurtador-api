ba7a439d92a3dad8d4f4065ba3e4a9fc5cca81d9
cryswerton <cryswertons@gmail.com>
2023-10-05 20:08:46 +0100

    added test a user can register

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    public function test_a_user_can_register()
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ];
+
+        $response = $this->post('/api/register', $userData);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('users', ['email' => $userData['email']]);
+    }
+}
